3709e7e62c87c4a756b6bfa2ff611a60060d692f
David Villacis <david@villacis.net>
2021-02-03 12:24:31 -0500

    Working TestDatasets module

--- src/TestDatasets.jl
+++ src/TestDatasets.jl
@@ -19,7 +19,6 @@ const remotedatasets = [
 function testdataset(datasetname; download_only::Bool = false)
 
     datasetfiles = dataset_path(full_datasetname(datasetname))
-    println(datasetfiles)
     download_only && return datasetfiles
     dataset = load_dataset(datasetfiles)
     return dataset
@@ -50,10 +49,23 @@ function full_datasetname(datasetname)
 end
 
 function dataset_path(datasetname)
-    ensure_artifact_installed("images", artifacts_toml)
+    ensure_artifact_installed("datasets", artifacts_toml)
 
-    dataset_dir = artifact_path(artifact_hash("images", artifacts_toml))
-    return joinpath(dataset_dir, datasetname)
+    dataset_dir = artifact_path(artifact_hash("datasets", artifacts_toml))
+    return joinpath(dataset_dir, "datasets" ,datasetname)
+end
+
+function load_dataset(datasetfiles)
+    image_pairs = readlines(joinpath(datasetfiles,"filelist.txt"))
+    M,N = size(load(joinpath(datasetfiles,split(image_pairs[1],",")[1])))
+    true_images = zeros(M,N,length(image_pairs))
+    data_images = zeros(M,N,length(image_pairs))
+    for i = 1:length(image_pairs)
+        pair = split(image_pairs[i],",")
+        true_images[:,:,i] = load(joinpath(datasetfiles,pair[1]))
+        data_images[:,:,i] = load(joinpath(datasetfiles,pair[2]))
+    end
+    return true_images,data_images
 end
 
 _findall(name; min_score=0.6) = findall(name, remotefiles,JaroWinkler(), min_score=min_score)
